02079b50d508fdf8ce750faf7e54dfd1bdcce5ff
Ozymendiazs <minebuildg@gmail.com>
2026-04-12 16:20:38 +0200

    adding the 'build.rs' file that build the rust code.

---
+++ Server/Relay/build.rs
@@ -0,0 +1,12 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+	let proto_file = "../Protocol/schematics/thorium.proto";
+	let includes = "../Protocol/schematics/";
+	prost_build::Config::new()
+		.compile_prostos(&[proto_file], &[includes])
+		.expect("Compilation error")
+
+	println!("cargo:return-if-changed={}", proto_file);
+}
