87df890fb0ab76c243104d5dc4afd100588d8246
Ozymendiazs <minebuildg@gmail.com>
2026-04-12 21:08:34 +0200

    i might crash out, named the function with the wrong name.

--- Server/Relay/build.rs
+++ Server/Relay/build.rs
@@ -5,7 +5,7 @@ fn main() {
 	let proto_file = "../Protocol/schematics/thorium.proto";
 	let includes = "../Protocol/schematics/";
 	prost_build::Config::new()
-		.compile_prostos(&[proto_file], &[includes])
+		.compile_protos(&[proto_file], &[includes])
 		.expect("Compilation error");
 
 	println!("cargo:return-if-changed={}", proto_file);
