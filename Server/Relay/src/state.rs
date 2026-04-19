075c9506fdcea2023996b5852b7efb3252444b67
Ozymendiazs <minebuildg@gmail.com>
2026-04-19 19:43:41 +0200

    adding the code to the state file that will handle the memory and connected user.

--- Server/Relay/src/state.rs
+++ Server/Relay/src/state.rs
@@ -1,1 +1,15 @@
-temp
+use std::{collections::HashMap, net::SocketAddr, sync::Arc};
+use tokio::sync::Mutex;
+
+#[derive(Clone)]
+pub struct ServerState {
+    pub peers: Arc<Mutex<HashMap<String, SocketAddr>>>,
+}
+
+impl ServerState {
+    pub fn new() -> Self {
+        Self {
+            peers: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+}
