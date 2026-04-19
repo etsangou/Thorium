3a069dae7e521ad5c7f9e4a7022139f805f54809
Ozymendiazs <minebuildg@gmail.com>
2026-04-19 20:49:41 +0200

    update the the main file so it can handle the configs.

--- Server/Relay/src/main.rs
+++ Server/Relay/src/main.rs
@@ -1,3 +1,4 @@
+mod config;
 mod handler;
 mod network;
 mod server;
@@ -11,10 +12,12 @@ use std::{error::Error, net::SocketAddr};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let addr: SocketAddr = "0.0.0.0:4433".parse()?;
+    let app_config = config::load();
+    let addr_str = format!("{}:{}", app_config.server.ip, app_config.server.port);
+    let addr: SocketAddr = addr_str.parse()?;
     
     let state = state::ServerState::new();
-    let server_config = network::configure_server()?;
+    let server_config = network::configure_server(&app_config)?;
     
     let endpoint = quinn::Endpoint::server(server_config, addr)?;
     println!("Signaling server listening on {}", endpoint.local_addr()?);
