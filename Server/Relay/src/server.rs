ac02c84cda28f13102c5ff827dd225505b1f969a
Ozymendiazs <minebuildg@gmail.com>
2026-04-19 20:31:10 +0200

    adding the code to the server file that handle the main loop.

--- Server/Relay/src/server.rs
+++ Server/Relay/src/server.rs
@@ -1,1 +1,17 @@
-temp
+use crate::{handler, state::ServerState};
+use quinn::Endpoint;
+
+pub async fn run(endpoint: Endpoint, state: ServerState) {
+    while let Some(conn) = endpoint.accept().await {
+        let state_clone = state.clone();
+        tokio::spawn(async move {
+            if let Ok(connection) = conn.await {
+                let remote_addr = connection.remote_address();
+                println!("incoming connection from {}", remote_addr);
+                handler::handle_connection(connection, state_clone).await;
+            } else {
+                println!("connection failed");
+            }
+        });
+    }
+}
