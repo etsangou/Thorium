5549b1ee83e58d3964409c1dca5156b715bebae0
Ozymendiazs <minebuildg@gmail.com>
2026-04-19 20:18:40 +0200

    adding the code to the network file that will handle the QUIC config and the TLS ceretificats.

--- Server/Relay/src/network.rs
+++ Server/Relay/src/network.rs
@@ -1,1 +1,21 @@
-temp
+use quinn::{ServerConfig, TransportConfig};
+use std::{error::Error, sync::Arc, time::Duration};
+
+pub fn configure_server() -> Result<ServerConfig, Box<dyn Error>> {
+    let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()])?;
+    let cert_der = rustls::Certificate(cert.serialize_der()?);
+    let priv_key = rustls::PrivateKey(cert.serialize_private_key_der());
+    
+    let crypto = rustls::ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(vec![cert_der], priv_key)?;
+
+    let mut transport_config = TransportConfig::default();
+    transport_config.max_idle_timeout(Some(Duration::from_secs(300).try_into().unwrap()));
+
+    let mut server_config = ServerConfig::with_crypto(Arc::new(crypto));
+    server_config.transport_config(Arc::new(transport_config));
+
+    Ok(server_config)
+}
