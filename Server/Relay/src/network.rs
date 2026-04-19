8d4a86fa10356f3b410950e047d96397e828160b
Ozymendiazs <minebuildg@gmail.com>
2026-04-19 20:48:59 +0200

    update to the network file, so it can handle the configs file.

--- Server/Relay/src/network.rs
+++ Server/Relay/src/network.rs
@@ -1,8 +1,9 @@
+use crate::config::Config;
 use quinn::{ServerConfig, TransportConfig};
 use std::{error::Error, sync::Arc, time::Duration};
 
-pub fn configure_server() -> Result<ServerConfig, Box<dyn Error>> {
-    let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()])?;
+pub fn configure_server(app_config: &Config) -> Result<ServerConfig, Box<dyn Error>> {
+    let cert = rcgen::generate_simple_self_signed(vec![app_config.server.cert_hostname.clone()])?;
     let cert_der = rustls::Certificate(cert.serialize_der()?);
     let priv_key = rustls::PrivateKey(cert.serialize_private_key_der());
     
@@ -12,7 +13,7 @@ pub fn configure_server() -> Result<ServerConfig, Box<dyn Error>> {
         .with_single_cert(vec![cert_der], priv_key)?;
 
     let mut transport_config = TransportConfig::default();
-    transport_config.max_idle_timeout(Some(Duration::from_secs(300).try_into().unwrap()));
+    transport_config.max_idle_timeout(Some(Duration::from_secs(app_config.network.idle_timeout_secs).try_into().unwrap()));
 
     let mut server_config = ServerConfig::with_crypto(Arc::new(crypto));
     server_config.transport_config(Arc::new(transport_config));
